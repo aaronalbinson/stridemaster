8f81e9731d23f02df6710174a65d702f4cebdffd
aaronalbinson <azza1990uk@yahoo.com>
2023-04-09 00:22:33 +0100

    Fix env connection

--- pages/api/fitbit/callback.js
+++ pages/api/fitbit/callback.js
@@ -1,20 +1,19 @@
 import fetch from 'node-fetch';
 import jwt from 'jsonwebtoken';
-import { FITBIT_CLIENT_ID, FITBIT_CLIENT_SECRET, FITBIT_REDIRECT_URI, FITBIT_TOKEN_URI } from '../../config';
 
 export default async function callback(req, res) {
   const { code } = req.query;
 
   const requestBody = new URLSearchParams();
-  requestBody.append('clientId', FITBIT_CLIENT_ID);
+  requestBody.append('clientId', process.env.NEXT_PUBLIC_FITBIT_CLIENT_ID);
   requestBody.append('grant_type', 'authorization_code');
-  requestBody.append('redirect_uri', FITBIT_REDIRECT_URI);
+  requestBody.append('redirect_uri', process.env.FITBIT_REDIRECT_URI);
   requestBody.append('code', code);
 
-  const response = await fetch(FITBIT_TOKEN_URI, {
+  const response = await fetch(process.env.FITBIT_TOKEN_URI, {
     method: 'POST',
     headers: {
-      Authorization: `Basic ${Buffer.from(`${FITBIT_CLIENT_ID}:${FITBIT_CLIENT_SECRET}`).toString('base64')}`,
+      Authorization: `Basic ${Buffer.from(`${process.env.NEXT_PUBLIC_FITBIT_CLIENT_ID}:${process.env.FITBIT_CLIENT_SECRET}`).toString('base64')}`,
       'Content-Type': 'application/x-www-form-urlencoded',
     },
     body: requestBody,
